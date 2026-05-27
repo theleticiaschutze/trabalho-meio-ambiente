2494f0a33dcdbc810481244dd1431f72e758ed06
dineagisele <dineagisele@gmail.com>
2026-05-27 17:35:11 -0300

    Upload site Dinea

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EcoCycle - Economia Circular</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <!-- MENU -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
+        <div class="container">
+            <a class="navbar-brand">
+                🌎 EcoVida
+            </a>
+            <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#economia">
+                            Economia Circular
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#reciclagem">Reciclagem</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#impactos">Impactos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#descarte">Descarte</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- BANNER -->
+    <div class="container-fluid p-0">
+        <img src="imagem/natureza.jpg" class="img-fluid w-100 banner">
+    </div>
+    <header class="container text-center mt-5">
+        <h1>Descarte Correto:Transformando lixo em oportunidade</h1>
+        <p class="lead">
+            Aprenda sobre economia circular e descubra como pequenas atitudes ajudam o planeta.
+        </p>
+    </header>
+    <section id="economia" class="container mt-5">
+        <h2>O que é Economia Circular?</h2>
+        <p>
+            Economia circular busca reduzir desperdícios,
+            reutilizar materiais e manter recursos em uso pelo maior tempo possível.
+        </p>
+        <p>Economia Linear:
+            Extrair → Produzir → Consumir → Descartar
+        </p>
+        <p>
+            Economia Circular:
+            Reduzir → Reutilizar → Reciclar
+        </p>
+    </section>
+    <section id="reciclagem" class="container mt-5">
+        <h2 class="text-center">O que é reciclagem</h2>
+        <div class="row align-items-center mt-4">
+            <div class="col-md-6">
+                <img src="imagem/reciclagem.jpg" class="img-fluid rounded shadow">
+            </div>
+            <div class="col-md-6">
+                <p>
+                    A Reciclagem blabla..........
+                </p>
+                <p>
+                    Separar corretamente...........
+                </p>
+                <p>
+                    Pequenas atitude contribuem..........
+                </p>
+            </div>
+        </div>
+    </section>
+    <section id="impactos" class="container mt-5">
+        <h2 class="text-center">Consequências do descarte incorreto</h2>
+        <div class="row mt-4">
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="imagem/lixo_oceano.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5>Oceanos Poluídos</h5>
+                        <p>
+                            O descarte incorreto de resíduos prejudica a vida marinha e o ecossistema.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="imagem/rio_poluido.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5>Rios Contaminados</h5>
+                        <p>
+                            Pilhas e resíduos eletrônicos podem liberar substâncias tóxicas.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="descarte" class="container mt-5">
+        <h2 class="text-center">Onde descartar?</h2>
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="card p-3 text-center">
+                    <h3>🔋</h3>
+                    <h5>Pilhas</h5>
+                    <p>
+                        Procure pontos de coleta específicos.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card p-3 text-center">
+                    <h3>💻</h3>
+                    <h5>Eletrônicos</h5>
+                    <p>
+                        Equipamentos possuem materiais recicláveis.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card p-3 text-center">
+                    <h3>📱</h3>
+                    <h5>Celulares</h5>
+                    <p>
+                        Nunca descarte no lixo comum.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center mt-5">
+            <h4>Locais Oficiais</h4>
+            <a
+                href="https://www.samaejs.com.br/central-do-usuario/pev/"
+                target="_blank" class="btn btn-success m-2">PEV SAMAE
+            </a>
+            <a
+                href="https://www.samaegaspar.com.br/servicos/residuos-solidos/ecoponto"
+                target="_blank" class="btn btn-primary m-2">Ecoponto
+            </a>
+            <a
+                href="https://www.ima.sc.gov.br/index.php/qualidade-ambiental/residuos-solidos/programa-penso-logo-destino"
+                target="_blank" class="btn btn-dark m-2">Programa Penso, Logo Destino
+            </a>
+        </div>
+    </section>
+    <footer class="bg-dark text-light text-center p-4 mt-5">
+        Disciplina:
+        Meio Ambiente, Trabalho e Sociedade
+        <br>
+        IFSC
+        <br>
+        Projeto EcoCycle
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
